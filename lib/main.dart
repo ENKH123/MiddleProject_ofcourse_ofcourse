14f6524526f6098a6a0ecfcec273479002a740be
yjkwon <casper118412@gmail.com>
2025-11-04 13:49:45 +0900

    alert screen complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:of_course/screens/login_screen.dart';
+import 'package:of_course/screens/alert_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LoginScreen(),
+      home: const AlertScreen(),
     );
   }
 }
