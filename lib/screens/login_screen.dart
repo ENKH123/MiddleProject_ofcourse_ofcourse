cfc5689c12f119fd22492863edaaa31cbe61d93c
yjkwon <casper118412@gmail.com>
2025-11-04 11:39:55 +0900

    backgroundColor change

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,6 +6,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffFAFAFA),
       body: SafeArea(
         child: Stack(
           children: [
