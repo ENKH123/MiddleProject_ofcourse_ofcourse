f9a0253e6061910b5a2b6969c28a683c5aa3d574
김재현 <kimjaehyun@gimjaehyeon-ui-MacBookPro.local>
2025-11-05 10:03:47 +0900

    fix(report): 누락된 부분 보완

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -1,7 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-
+import 'package:image_picker/image_picker.dart';
+import 'package:cross_file/cross_file.dart';
+import 'dart:io';
 import '../models/report_models.dart';
 
 ///ID: FO_03_03_01
