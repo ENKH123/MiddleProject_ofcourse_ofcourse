faaca932cbd87e0b3623236d1d27bf6206f1c679
EomSB <pejesw236@gmail.com>
2025-11-11 15:53:57 +0900

    4 change file by sb

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -71,3 +71,153 @@ Future<ThemeMode?> showThemeModeDialog(
     },
   );
 }
+
+/// 라이트 테마
+final ThemeData lightTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme:
+      ColorScheme.fromSeed(
+        seedColor: const Color(0xFF003366), // 메인 포인트 색
+        brightness: Brightness.light,
+      ).copyWith(
+        surface: const Color(0xFFFAFAFA),
+        //background: const Color(0xFFFAFAFA),
+        primary: const Color(0xFF003366),
+        onPrimary: const Color(0xFFFAFAFA), // 버튼 텍스트 색
+        onSurface: const Color(0xFF030303), // 기본 텍스트 색
+      ),
+
+  scaffoldBackgroundColor: const Color(0xFFFAFAFA),
+  canvasColor: const Color(0xFFFAFAFA),
+
+  // surfaceTintColor: Colors.transparent,
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(color: Color(0xFF030303)),
+    bodyMedium: TextStyle(color: Color(0xFF030303)),
+    titleLarge: TextStyle(
+      color: Color(0xFF030303),
+      //fontWeight: FontWeight.bold,
+    ),
+  ),
+
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: const Color(0xFF003366), // 버튼 배경
+      foregroundColor: const Color(0xFFFAFAFA), // 버튼 글자색
+      //textStyle: const TextStyle(fontWeight: FontWeight.bold),
+      //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  ),
+
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      foregroundColor: const Color(0xFF003366),
+      //textStyle: const TextStyle(fontWeight: FontWeight.w500),
+    ),
+  ),
+
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color(0xFFFAFAFA),
+    foregroundColor: Color(0xFF030303),
+    elevation: 0,
+  ),
+  filledButtonTheme: FilledButtonThemeData(
+    style: FilledButton.styleFrom(
+      backgroundColor: const Color(0xFF003366),
+      foregroundColor: const Color(0xFFFAFAFA),
+      //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  ),
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      foregroundColor: const Color(0xFF003366),
+      //side: const BorderSide(color: Color(0xFF003366)),
+      //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  ),
+  iconButtonTheme: const IconButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: WidgetStatePropertyAll(Color(0xFF003366)),
+    ),
+  ),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: Color(0xFF003366),
+    foregroundColor: Color(0xFFFAFAFA),
+  ),
+);
+
+/// 다크 테마
+final ThemeData darkTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme(
+    brightness: Brightness.dark,
+    primary: const Color(0xFFC5D5E4), // 버튼 기본 색
+    onPrimary: const Color(0xFF030303), // 버튼 텍스트 색
+    secondary: const Color(0xFFC5D5E4),
+    onSecondary: const Color(0xFF030303),
+    error: Colors.redAccent,
+    onError: const Color(0xFFFAFAFA),
+    //background: const Color(0xFF003366), // 전체 배경색
+    //onBackground: const Color(0xFFFAFAFA), // 기본 글자색
+    surface: const Color(0xFF003366),
+    onSurface: const Color(0xFFFAFAFA),
+  ),
+
+  scaffoldBackgroundColor: const Color(0xFF003366),
+  canvasColor: const Color(0xFF003366),
+
+  //surfaceTintColor: Colors.transparent,
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(color: Color(0xFFFAFAFA)),
+    bodyMedium: TextStyle(color: Color(0xFFFAFAFA)),
+    titleLarge: TextStyle(
+      color: Color(0xFFFAFAFA),
+      //fontWeight: FontWeight.bold,
+    ),
+  ),
+
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: const Color(0xFFC5D5E4), // 버튼 배경
+      foregroundColor: const Color(0xFF030303), // 버튼 글자색
+      //textStyle: const TextStyle(fontWeight: FontWeight.bold),
+      //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  ),
+
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      foregroundColor: const Color(0xFFC5D5E4),
+      //textStyle: const TextStyle(fontWeight: FontWeight.w500),
+    ),
+  ),
+
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color(0xFF003366),
+    foregroundColor: Color(0xFFFAFAFA),
+    elevation: 0,
+  ),
+  filledButtonTheme: FilledButtonThemeData(
+    style: FilledButton.styleFrom(
+      backgroundColor: const Color(0xFFC5D5E4),
+      foregroundColor: const Color(0xFF030303),
+      //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  ),
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      foregroundColor: const Color(0xFFC5D5E4),
+      //side: const BorderSide(color: Color(0xFFC5D5E4)),
+      //shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  ),
+  iconButtonTheme: const IconButtonThemeData(
+    style: ButtonStyle(
+      foregroundColor: WidgetStatePropertyAll(Color(0xFFC5D5E4)),
+    ),
+  ),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: Color(0xFFC5D5E4),
+    foregroundColor: Color(0xFF030303),
+  ),
+);
