959f8c695eed166393f171f4eef19dbaa7f6cc2c
yjkwon <casper118412@gmail.com>
2025-11-13 17:46:58 +0900

    알림 아이콘 실시간 변경
    supabasr realtime 적용

--- lib/feature/alert/viewmodels/alert_viewmodel.dart
+++ lib/feature/alert/viewmodels/alert_viewmodel.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:of_course/core/managers/supabase_manager.dart';
 import 'package:of_course/core/models/alert_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AlertViewModel extends ChangeNotifier {
   List<AlertModel>? _alerts;
   List<AlertModel>? get alerts => _alerts;
 
+  late RealtimeChannel channel;
+
   AlertViewModel() {
     fetchAlerts();
+    channel = _subscribeAlertEvent();
+  }
+
+  @override
+  void dispose() {
+    channel.unsubscribe();
+    super.dispose();
   }
 
   Future<void> fetchAlerts() async {
@@ -17,5 +27,23 @@ class AlertViewModel extends ChangeNotifier {
 
   Future<void> deleteAlert(int alertId) async {
     await SupabaseManager.shared.deleteAlert(alertId);
+  }
+
+  RealtimeChannel _subscribeAlertEvent() {
+    return SupabaseManager.shared.supabase
+        .channel('listen_alert')
+        .onPostgresChanges(
+          event: PostgresChangeEvent.insert,
+          schema: 'public',
+          table: 'alert',
+          callback: (payload) {
+            final newAlert = payload.newRecord;
+            final AlertModel newChatMessage = AlertModel.fromJson(newAlert);
+            print('Change received: ${payload.toString()}');
+            _alerts?.add(newChatMessage);
+            notifyListeners();
+          },
+        )
+        .subscribe();
   }
 }
