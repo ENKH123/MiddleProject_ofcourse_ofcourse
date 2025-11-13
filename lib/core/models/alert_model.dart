ff11c1d7a8abc2a6ff4cbab34765f9621696dd25
yjkwon <casper118412@gmail.com>
2025-11-13 10:48:38 +0900

    알림 가져오기 완료
    보낸 사람 닉네임 표시
    받는 사람이 본인인 알림만 출력
    보낸 사람이 본인인 알림은 제외

--- lib/core/models/alert_model.dart
+++ lib/core/models/alert_model.dart
@@ -5,6 +5,7 @@ class AlertModel {
   final String type;
   final int course_id;
   final DateTime created_at;
+  final String fromUserNickname;
 
   AlertModel({
     required this.id,
@@ -13,9 +14,12 @@ class AlertModel {
     required this.type,
     required this.course_id,
     required this.created_at,
+    required this.fromUserNickname,
   });
 
   factory AlertModel.fromJson(Map<String, dynamic> json) {
+    final usersData = json["users"] as Map<String, dynamic>?;
+    final nickname = usersData?["nickname"] as String? ?? '알 수 없음';
     return AlertModel(
       id: json["id"] as int,
       from_user_id: json["from_user_id"] as String,
@@ -23,6 +27,7 @@ class AlertModel {
       type: json["type"] as String,
       course_id: json["course_id"] as int,
       created_at: DateTime.parse(json['created_at'] as String),
+      fromUserNickname: nickname,
     );
   }
 }
