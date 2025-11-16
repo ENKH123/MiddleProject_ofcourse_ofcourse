591f84e0b9023152f95cb08d38e6d12092782454
EomSB <pejesw236@gmail.com>
2025-11-17 01:49:54 +0900

    테마변경_저장된코스 상단

--- lib/feature/course/screens/liked_course_page.dart
+++ lib/feature/course/screens/liked_course_page.dart
@@ -77,41 +77,50 @@ class _LikedCoursePageState extends State<LikedCoursePage> {
                   children: tagList.map((tag) {
                     final bool isSelected = _isSelected(tag);
 
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 10),
-                      child: GestureDetector(
-                        onTap: () => _toggleCategory(tag),
-                        child: AnimatedContainer(
-                          height: 35,
-                          alignment: Alignment.center,
-                          duration: const Duration(milliseconds: 200),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 18,
-                            vertical: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            color: isSelected
-                                ? const Color(0xff003366)
-                                : Colors.white,
-                            borderRadius: BorderRadius.circular(30),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.05),
-                                blurRadius: 5,
-                                offset: const Offset(0, 2),
+                    return Builder(
+                      builder: (context) {
+                        final cs = Theme.of(context).bottomNavigationBarTheme;
+                        final sc = Theme.of(context).colorScheme;
+
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: GestureDetector(
+                            onTap: () => _toggleCategory(tag),
+                            child: AnimatedContainer(
+                              height: 35,
+                              alignment: Alignment.center,
+                              duration: const Duration(milliseconds: 200),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 18,
+                                vertical: 10,
                               ),
-                            ],
-                          ),
-                          child: Text(
-                            tag.name,
-                            style: TextStyle(
-                              color: isSelected ? Colors.white : Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
+                              decoration: BoxDecoration(
+                                color: isSelected
+                                    ? cs.selectedItemColor
+                                    : cs.unselectedItemColor,
+                                borderRadius: BorderRadius.circular(30),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.05),
+                                    blurRadius: 5,
+                                    offset: const Offset(0, 2),
+                                  ),
+                                ],
+                              ),
+                              child: Text(
+                                tag.name,
+                                style: TextStyle(
+                                  color: isSelected
+                                      ? sc.onPrimary
+                                      : Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
+                        );
+                      },
                     );
                   }).toList(),
                 ),
