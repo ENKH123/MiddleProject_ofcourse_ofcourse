e0265aa28ae168cfef1130226457579304fd8cd5
yjkwon <casper118412@gmail.com>
2025-11-18 16:57:29 +0900

    온보딩 기능 완료
    ui만 작업하면 됨

--- lib/feature/auth/screens/onboarding_screen.dart
+++ lib/feature/auth/screens/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:of_course/feature/auth/viewmodels/login_viewmodel.dart';
 import 'package:of_course/feature/auth/viewmodels/onboarding_viewmodel.dart';
 import 'package:provider/provider.dart';
 
@@ -19,13 +20,14 @@ class _OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final viewmodelL = context.read<LoginViewModel>();
     return Consumer<OnboardingViewModel>(
       builder: (context, viewmodel, child) {
         return Stack(
           children: [
             PageView.builder(
               controller: viewmodel.pageController,
-              itemCount: viewmodel.maxPage,
+              itemCount: viewmodel.totalPages,
               onPageChanged: (int newPage) {
                 viewmodel.updatePage(newPage);
               },
@@ -44,7 +46,9 @@ class _OnboardingScreen extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     GestureDetector(
-                      onTap: () {},
+                      onTap: () {
+                        viewmodelL.finishOnboarding();
+                      },
                       child: const Text(
                         "SKIP",
                         style: TextStyle(
@@ -55,12 +59,30 @@ class _OnboardingScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                    SizedBox(
-                      width: double.maxFinite,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("시작하기"),
-                      ),
+                    Column(
+                      spacing: 12,
+                      children: [
+                        DotsIndicator(
+                          currentPage: viewmodel.currentPage,
+                          totalPages: viewmodel.totalPages,
+                        ),
+                        SizedBox(
+                          width: double.maxFinite,
+                          child: ElevatedButton(
+                            onPressed:
+                                viewmodel.currentPage < viewmodel.totalPages - 1
+                                ? () {
+                                    viewmodel.goToNextPage();
+                                  }
+                                : () {
+                                    viewmodelL.finishOnboarding();
+                                  },
+                            child: viewmodel.currentPage >= 3
+                                ? const Text("시작하기")
+                                : const Text("다음"),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
@@ -73,6 +95,43 @@ class _OnboardingScreen extends StatelessWidget {
   }
 }
 
+// 인디케이터
+class DotsIndicator extends StatelessWidget {
+  final int currentPage;
+  final int totalPages;
+
+  const DotsIndicator({
+    super.key,
+    required this.currentPage,
+    required this.totalPages,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 8,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(totalPages, (index) => _buildDot(index, context)),
+    );
+  }
+
+  Widget _buildDot(int index, BuildContext context) {
+    bool isActive = index == currentPage;
+
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 150),
+      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+      height: isActive ? 10.0 : 8.0,
+      width: isActive ? 10.0 : 8.0,
+      decoration: BoxDecoration(
+        color: isActive ? Color(0xFF003366) : Colors.grey.shade400,
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+    );
+  }
+}
+
+// 온보딩 화면
 class OnboardingBox extends StatelessWidget {
   final OnboardingViewModel viewmodel;
   const OnboardingBox({super.key, required this.viewmodel});
